24cca8cc7e1fa82f6fa838fcdf79f9eeb38516e5
mhdtahawi <mhdtahawi@users.noreply.github.com>
2020-04-19 19:57:01 +0000

    deploy: 40f19442074f8dc9e719a8c07fd94f421c39a548

---
+++ precache-manifest.03a81503629ec99e24ca6c85ba0aa19c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8486d5eca42d96c1847520e12054f746",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "683b433b5f735d236fb6",
+    "url": "/material-dashboard-react/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "6ac64ce1524acbf19ee6",
+    "url": "/material-dashboard-react/static/css/main.36501932.chunk.css"
+  },
+  {
+    "revision": "683b433b5f735d236fb6",
+    "url": "/material-dashboard-react/static/js/2.70bf7200.chunk.js"
+  },
+  {
+    "revision": "1bc4652f39d67028ed3fdd3981be1217",
+    "url": "/material-dashboard-react/static/js/2.70bf7200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ac64ce1524acbf19ee6",
+    "url": "/material-dashboard-react/static/js/main.30d93643.chunk.js"
+  },
+  {
+    "revision": "7269b0c4207b44d8a9e4f0b85c8a81d2",
+    "url": "/material-dashboard-react/static/js/main.30d93643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5e5cb6c3fdf714eb4e2",
+    "url": "/material-dashboard-react/static/js/runtime-main.a5dbcd8f.js"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
